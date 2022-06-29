43d7e7a3ba752082f67ff839d27a0a7f6c17a452
Eresh Gorantla <eresh.gorantla@getir.com>
2022-06-29 15:09:26 +0530

    Added Curl commands.

--- src/main/java/com/zinkworks/atm/common/IErrorMessages.java
+++ src/main/java/com/zinkworks/atm/common/IErrorMessages.java
@@ -3,7 +3,7 @@ package com.zinkworks.atm.common;
 public interface IErrorMessages {
   String UN_EXPECTED_ERROR ="Unexpected Error Occurred";
   String ACCOUNT_NUMBER_NOT_FOUND = "The Account Number => %s, not found";
-  String ACCOUNT_HAS_INSUFFICIENT_FUNDS = "The Account Number => %s, didn't had enough to dispense";
+  String ACCOUNT_HAS_INSUFFICIENT_FUNDS = "The Account Number => %s, didn't had enough cash to dispense";
   String INVALID_PIN = "The provided PIN is not correct";
   String REQUESTED_AMOUNT_IS_NOT_AVAILABLE_IN_ATM = "ATM balance is low. Can't dispense the requested amount.";
   String UNAVAILABILITY_OF_DENOMINATION = "ATM Doesn't had requested amount in denominations";
